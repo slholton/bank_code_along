a9a4ad13b1f1ffd12434c6fba41ae25ced48f1bd
Shavonne Holton <shavonne.holton@gmail.com>
2021-04-24 20:37:58 -0700

    updated the html to test and include all of the code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let button = document.getElementById("transaction-button")
-button.onclick = (e) => {
+let form = document.getElementById("transaction-form")
+form.onsubmit = (e) => {
+    console.log(e.target["transaction-type"].value)
+    console.log(e.target.amount.value)
+
     e.preventDefault()
-    console.log("button")
 }
